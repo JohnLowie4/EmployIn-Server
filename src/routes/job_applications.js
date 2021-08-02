d110c17017f2e61d34ebf553b35bcd226b69ba72
John <jzlowie@gmail.com>
2021-08-02 16:40:41 -0600

    Added new GET id requests

--- src/routes/job_applications.js
+++ src/routes/job_applications.js
@@ -14,6 +14,21 @@ const job_applications = (db) => {
     });
   });
 
+  // id GET request for job_application info
+  route.get("/:id", (req, res) => {
+    const job_application_id = req.params.id;
+    db.query(
+      `
+        SELECT * FROM job_applications WHERE id = $1;
+      `,
+      [job_application_id]
+    ).then((response) => {
+      res.json(response.rows[0]);
+    }).catch((error) => {
+      res.json(error.message);
+    });
+  });
+
   /**
    * Future requests will be added here as the project progresses
    */
--- src/routes/job_posting.js
+++ src/routes/job_posting.js
@@ -14,6 +14,21 @@ const job_posting = (db) => {
     });
   });
 
+  // id GET request for job_posting infor
+  route.get("/:id", (req, res) => {
+    const job_posting_id = req.params.id;
+    db.query(
+      `
+        SELECT * FROM job_posting WHERE id = $1;
+      `,
+      [job_posting_id]
+    ).then((response) => {
+      res.json(response.rows[0]);
+    }).catch((error) => {
+      res.json(error.message);
+    });
+  });
+
   /**
    * Future requests will be added here as the project progresses
    */

5ad2f269923fc90460857c420147999a30b9c047
Chulainn1 <leam.murphy1@gmail.com>
2021-08-06 14:41:08 -0600

    applicant_links table now has links for all 20 users

--- src/db/seeds/03_applicant_links.sql
+++ src/db/seeds/03_applicant_links.sql
@@ -3,12 +3,82 @@ VALUES
 (1, 'https://github.com/'),
 (1, 'https://ca.linkedin.com/'),
 (1, 'https://personalblog.com/'),
+-- 
 (2, 'http://creddle.io/'),
 (2, 'https://twitter.com/'),
+-- 
 (3, 'https://github.com/'),
 (3, 'https://ca.linkedin.com/'),
+-- 
 (4, 'https://personalblog.com/'),
 (4, 'http://creddle.io/'),
+-- 
 (5, 'https://twitter.com/'),
 (5, 'https://github.com/'),
-(5, 'https://ca.linkedin.com/');
+(5, 'https://ca.linkedin.com/'),
+-- 
+(6, 'https://ca.linkedin.com/'),
+(6, 'https://github.com/'),
+(6, 'https://twitter.com/'),
+-- 
+(7, 'https://ca.linkedin.com/'),
+(7, 'https://github.com/'),
+(7, 'https://personalblog.com/'),
+-- 
+(8, 'https://ca.linkedin.com/'),
+(8, 'https://github.com/'),
+(8, 'https://personalblog.com/'),
+-- 
+(9, 'https://ca.linkedin.com/'),
+(9, 'https://github.com/'),
+(9, 'https://twitter.com/'),
+-- 
+(10, 'https://ca.linkedin.com/'),
+(10, 'https://github.com/'),
+(10, 'https://www.youtube.com/'),
+-- 
+(11, 'https://ca.linkedin.com/'),
+(11, 'https://github.com/'),
+(11, ''),
+-- 
+(12, 'https://ca.linkedin.com/'),
+(12, 'https://github.com/'),
+(12, 'https://twitter.com/'),
+-- 
+(13, 'https://ca.linkedin.com/'),
+(13, 'https://github.com/'),
+(13, 'https://www.youtube.com/'),
+-- 
+(14, 'https://ca.linkedin.com/'),
+(14, 'https://github.com/'),
+(14, 'https://medium.com/'),
+-- 
+(15, 'https://ca.linkedin.com/'),
+(15, 'https://github.com/'),
+(15, 'https://twitter.com/'),
+-- 
+(16, 'https://ca.linkedin.com/'),
+(16, 'https://github.com/'),
+(16, 'https://personalblog.com/'),
+-- 
+(17, 'https://ca.linkedin.com/'),
+(17, 'https://github.com/'),
+(17, 'https://medium.com/'),
+-- 
+(18, 'https://ca.linkedin.com/'),
+(18, 'https://github.com/'),
+(18, 'https://twitter.com/'),
+-- 
+(18, 'https://ca.linkedin.com/'),
+(18, 'https://github.com/'),
+(18, 'https://www.youtube.com/'),
+-- 
+(19, 'https://ca.linkedin.com/'),
+(19, 'https://github.com/'),
+(19, 'https://medium.com/'),
+-- 
+(20, 'https://ca.linkedin.com/'),
+(20, 'https://github.com/'),
+(20, 'https://twitter.com/');
+
+
